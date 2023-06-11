fefc4d1dfbecae622c37bac5f1935aa640b984c2
KevinMarcos26 <kevinmarcos_96@yahoo.com.ar>
2023-06-11 13:02:24 -0300

    commit1

---
+++ db/init_db.py
@@ -0,0 +1,12 @@
+# inicializar la base de datos (Solo se requiere una vez)
+import sqlite3
+
+
+def db_create():
+    connection = sqlite3.connect("database.db")
+
+    with open("db/schema.sql") as f:
+        connection.executescript(f.read())
+
+    connection.commit()
+    connection.close()
